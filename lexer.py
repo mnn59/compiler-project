5eb44a29f9b5d517ad0a29117da2e59760f7cc77
mahdi niknejad <m.niknejad@aut.ac.ir>
2020-10-27 00:42:37 -0700

    First commit

---
+++ lexer.py
@@ -0,0 +1,82 @@
+from ply import lex
+from ply.lex import TOKEN
+
+'''
+if (002 + 13>(42*2) + 5) {
+    while (3 < 4/7 - 5) {
+        print(3);
+    }
+}
+'''
+
+class Lexer:
+    # LRB: ( , RRB: ) , LCB: { , RCB: } , LT: < , GT: >
+    # new line
+    tokens = [
+        'IF', 'WHILE', 'PRINT',
+        'LRB', 'RRB', 'LCB', 'RCB',
+        'INTEGER', 'SUM', 'SUB', 'MUL', 'DIV',
+        'LT', 'GT', 'SEMICOLON'
+    ]
+
+    # COLONS
+    t_SEMICOLON = r'\;'
+    # BRACKETS
+    t_LRB = r'\('
+    t_RRB = r'\)'
+    t_LCB = r'\{'
+    t_RCB = r'\}'
+    # OPERATOR
+    t_SUM = r'\+'
+    t_SUB = r'\-'
+    t_MUL = r'\*'
+    t_DIV = r'\/'
+    t_LT = r'\<'
+    t_GT = r'\>'
+    # RESERVED KEYWORD
+    t_IF = r'if'
+    t_WHILE = r'while'
+    t_PRINT = r'print'
+
+    # digit = r'([1-9])'
+    # zero = r'([0])'
+    # var = r'(' + digit + r'(' + digit + r'|' + zero + r')*)'
+    #
+    # @TOKEN(var)
+    def t_INTEGER(self, t):
+        # for me
+        # r"""[+|-]?[1-9]([1-9]|[0])*"""
+        # t.value = int(t.value)
+
+        r"""[0-9]+"""
+        t.value = int(t.value)
+
+        # r"""[0-9]{1,9}"""
+        # t.value = int(t.value)
+
+        # r'((?!^0\d+)^0)|((?!0+)\d{1,10})'
+        # r'\#[-\+]?([1-9]\d*|0)'      # for robati
+
+        # r'[-|+]?(\d+)'
+        return t
+
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    # characters that we want from lexer to skip theme
+    # NOTE: it must be lower case 'ignore' not 'IGNORE'
+    t_ignore = '\n \t'
+
+    # NOTE: t.value = lexeme  t.type = token
+    # NOTE: it must be lower case 'error' not 'ERROR'
+    def t_error(self, t):
+        raise Exception('Error at ', t.value)
+
+    def build(self, **kwargs):
+        self.lexer = lex.lex(self, **kwargs)
+        return self.lexer
+
+# issues:
+# 1. 002 --> 2
+# 2. 2+3 --> 2 + 3
---
+++ main.py
@@ -0,0 +1,12 @@
+from lexer import Lexer
+
+lexer = Lexer().build()
+file = open('old-test.txt')
+text_input = file.read()
+file.close()
+lexer.input(text_input)
+while True:
+    tok = lexer.token()
+    if not tok:  # EOF(end of file)
+        break
+    print(tok)
