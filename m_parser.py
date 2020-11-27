4db3fdf5d52b1be9f6a9dd77bcbf6dc361d97f9e
amirhaf76 <amir.haf76@gmail.com>
2020-11-27 11:38:14 +0330

    Some other parts were added.

--- m_parser.py
+++ m_parser.py
@@ -13,12 +13,18 @@ class Parser:
 
     def p_program(self, p):
         'program : expression'
-
+        # p[0] = p[1]
         print('program : expression')
 
-    # def p_program_rel(self, p):
-    #     'expression : relopexp'
-    #     print('expression : relopexp')
+    def p_program_val(self, p):
+        'expression : lvalue'
+        # p[0] = p[1]
+        print('expression : lvalue')
+
+    def p_program_val_exp(self, p):
+        'expression : lvalue ASSIGN expression'
+        # p[0] = p[1]
+        print('expression : lvalue')
 
     def p_expression_plus(self, p):
         'expression : expression SUM term'
@@ -104,6 +110,85 @@ class Parser:
         'expression : expression NE expression'
         p[0] = p[1] != p[3]
         print('expression : expression NE expression')
+
+    def p_id_exp(self, p):
+        'lvalue : ID'
+        p[0] = p[1]
+        print('lvalue : ID')
+
+    def p_id_SB(self, p):
+        'lvalue : ID LSB expression RSB'
+        p[0] = p[2]
+        print('lvalue : LSB ID RSB')
+
+    def p_block(self, p):
+        'block : LCB stmtlist RCB'
+        p[0] = p[2]
+        print('block : LCB stmtlist RCB')
+
+    def p_stmt_block(self, p):
+        'stmt : block'
+        p[0] = p[1]
+        print('stmt : block')
+
+    def p_stmt_print(self, p):
+        'stmt : PRINT LRB ID RRB SEMICOLON'
+        p[0] = f'print({p[3]});'
+        print('stmt : PRINT LRB ID RRB SEMICOLON')
+
+    def p_stmt_exp(self, p):
+        'stmt : RETURN expression SEMICOLON'
+        p[0] = p[2]
+        print('stmt : RETURN expression SEMICOLON')
+
+    def p_stmtlist_stmt(self, p):
+        'stmtlist : stmt'
+        p[0] = p[1]
+        print('stmtlist : stmt')
+
+    def p_stmtlist_empty(self, p):
+        'stmtlist : empty'
+        print('stmtlist : empty')
+
+    def p_stmtlist_stmtlist(self, p):
+        'stmtlist : stmtlist stmt'
+        p[0] = p[1] + p[2]
+        print('stmtlist : stmtlist stmt')
+
+    def p_case(self, p):
+        'case : WHERE factor COLON stmtlist'
+        print('case : WHERE factor COLON stmtlist')
+
+    def p_cases_case(self, p):
+        'cases : case'
+        p[0] = p[1]
+        print('cases : case')
+
+    def p_cases_cases(self, p):
+        'cases : cases case'
+        p[0] = p[1] + p[2]
+        print('cases : cases case')
+
+    def p_cases_empty(self, p):
+        'cases : empty'
+        print('cases : empty')
+
+    def p_explist_exp(self, p):
+        'explist : exp'
+        p[0] = p[1]
+        print('explist : exp')
+
+    def p_explist_explist(self, p):
+        'explist : explist COMMA expression'
+        p[0] = f'{p[1]},{p[3]}'
+        print('explist : explist COMMA expression')
+
+    def p_empty(self, p):
+        'empty :'
+        print('empty :')
+        pass
+    # def p_if_exp(self, p):
+    #     'ifexp : LRB expression RRB '
 
     # def p_relop_gt(self, p):
     #     'relopexp : relopexp GT expression'
